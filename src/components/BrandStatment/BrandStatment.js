59e5d9e97c119969eea47b87e7b213af05693531
Roger Campbell II <rc1336@gmail.com>
2019-03-05 14:30:48 -0500

    Figured out how to use the Gatsby Img tag. Have my headshot rendering. Need to reaccess what to do next.

---
+++ src/components/BrandStatment/BrandStatment.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BrandStatment extends Component {
+    render() {
+        return (
+            <div>
+                <p>Mission driven Software Engineer equipped with the insight to thrive under pressure and tackle complex problems. I bring a creative edge and an innovative mindset to development. </p>
+            </div>
+        );
+    }
+}
+
+export default BrandStatment;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import BrandStatment from '../components/BrandStatment/BrandStatment'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Whats up mutha sucka</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <BrandStatment />
+
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
