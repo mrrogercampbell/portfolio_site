9f0c1e82586a489eba1e83ac9aeffaaf42d687c1
Roger Campbell II <rc1336@gmail.com>
2019-03-05 13:37:40 -0500

    Initial commit. Create Gatsby app and NavBar component. Next: steps build it out.

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Link } from "gatsby"
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <nav>
+
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default NavBar;
