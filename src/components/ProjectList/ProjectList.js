d10e8dfad6436e926d6c0de3e6363972fc54a234
Roger Campbell II <rc1336@gmail.com>
2019-03-06 00:20:30 -0500

    Built out static ProjectList component and add styling to make the list render vertically. Still need to figure out how to lessen the width of the main container div. Feel like I should use bootstrap here, also like the idea of using pure css; we; will have to wait and see. Next Step: Create and build out MockUp Component.

---
+++ src/components/ProjectList/ProjectList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './ProjectList.css'
+
+class ProjectList extends Component {
+    render() {
+        return (
+            <div className='project-list'>
+                <ul>
+                    <li>PlaceHolder 1</li>
+                    <li>PlaceHolder 2</li>
+                    <li>PlaceHolder 3</li>
+                    <li>PlaceHolder 4</li>
+                    <li>PlaceHolder 5</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default ProjectList;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,10 @@ import Layout from "../components/layout"
 import Image from "../components/Image/image"
 import SEO from "../components/seo"
 import BrandStatment from '../components/BrandStatment/BrandStatment'
+import ProjectList from '../components/ProjectList/ProjectList'
+
 import '../components/Image/image.css'
+
 
 const IndexPage = () => (
   <Layout>
@@ -15,6 +18,7 @@ const IndexPage = () => (
     <div className='img-comp' style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <ProjectList />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
