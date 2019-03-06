840e8427050f71a39d4fedc8dcd14a449eb8816c
Roger Campbell II <rc1336@gmail.com>
2019-03-06 00:08:07 -0500

    placed borders around each created component. Not sure what to do with the footer yet so I left it alone. Also created ProjectList dir, .js, and .css.  Next Step: Build out static ProjectList Component. Then: make MockUp Component placeholders.

--- src/components/BrandStatment/BrandStatment.js
+++ src/components/BrandStatment/BrandStatment.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import './BrandStatment.css'
 
 class BrandStatment extends Component {
     render() {
         return (
-            <div>
+            <div className='brand-statement'>
                 <p>Mission driven Software Engineer equipped with the insight to thrive under pressure and tackle complex problems. I bring a creative edge and an innovative mindset to development. </p>
             </div>
         );
