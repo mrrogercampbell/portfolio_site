1872c96d92043d4966a03fb65a660982363e1fdc
Roger Campbell II <rc1336@gmail.com>
2019-03-05 23:56:00 -0500

    Created Image dir and image.css file. Gave Image component a class which put a green boarder around it. Next Step: place borders around each component you have built and match the correspond choices from OneNote Outline.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Image from "../components/Image/image"
 import SEO from "../components/seo"
 import BrandStatment from '../components/BrandStatment/BrandStatment'
+import '../components/Image/image.css'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <BrandStatment />
 
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div className='img-comp' style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
